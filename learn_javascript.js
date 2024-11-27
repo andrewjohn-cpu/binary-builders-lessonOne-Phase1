7987211dc67994980cd00b4cab16ba65b8bd2c4c
andyjohn23 <andrewnick906@gmail.com>
2024-11-27 08:40:38 +0300

    add notes

---
+++ learn_javascript.js
@@ -0,0 +1,21 @@
+// var myName = "VICTOR";
+// let mySecondName = "diana";
+// const myLocation = "nairobi";
+
+// console.log(myName.toLowerCase());
+// console.log(mySecondName.length);
+// console.log(myLocation.toUpperCase());
+
+// let username = "victor";
+// let password = 123456789;
+
+// if (username === "victor") {
+//   console.log("Welcome, ");
+// } else {
+//   console.log("Wrong username");
+// }
+
+
+(function(){
+    console.log("testing this.")
+})();
